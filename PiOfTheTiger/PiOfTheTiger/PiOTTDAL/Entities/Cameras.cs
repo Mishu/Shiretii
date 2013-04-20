c03c8d627553b1cdb356d4140ec55895d9b227f1
Bogdan <oak_s_acorn@yahoo.com>
2013-04-20 22:38:18 +0300

    Add WebCamProject

---
+++ PiOfTheTiger/PiOfTheTiger/PiOTTDAL/Entities/Cameras.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiOTTDAL.Entities
+{
+    public class Camera
+    {
+        public int IdCamera { get; set; }
+
+        [Name]
+        public string CameraName { get; set; }
+        public string Path { get; set; }
+    }
+}
---
+++ PiOfTheTiger/PiOfTheTiger/PiOTTWebCam/CaptureImage/CaptureImage.cs
@@ -0,0 +1,20 @@
+﻿using PiOTTDAL.Entities;
+using RaspberryCam;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiOTTWebCam.CaptureImage
+{
+    public class CaptureImage
+    {
+        public void TakePicture(Camera camera)
+        {
+            var cameras = Cameras.DeclareDevice().Named(camera.CameraName).WithDevicePath(camera.Path).AndDevice().Memorize();
+            
+            cameras.Get(camera.CameraName).SavePicture(new PictureSize(640, 480), "Test1.jpg", 20);
+        }
+    }
+}
