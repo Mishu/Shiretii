3439bc696fd91fcfbba00f418bdc51464e922203
Bogdan <oak_s_acorn@yahoo.com>
2013-04-20 17:10:36 +0300

    Add querying class

--- PiOfTheTiger/PiOfTheTiger/PiOTTDAL/Entities/AddressBook.cs
+++ PiOfTheTiger/PiOfTheTiger/PiOTTDAL/Entities/AddressBook.cs
@@ -8,6 +8,14 @@ namespace PiOTTDAL.Entities
 {
     public class AddressBook
     {
+        private int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+        
         public int IdAddressBook { get; set; }
         public string EmailAddress { get; set; }
         public string AddressName { get; set; }
