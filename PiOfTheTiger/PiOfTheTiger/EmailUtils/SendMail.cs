51a160a7381e58068dda9fbb5892d66b51578a00
Bogdan <oak_s_acorn@yahoo.com>
2013-04-20 14:06:59 +0300

    Add Entities for EMail messaging

--- PiOfTheTiger/PiOfTheTiger/EmailUtils/SendMail.cs
+++ PiOfTheTiger/PiOfTheTiger/EmailUtils/SendMail.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EmailUtils
 {
-    public class Class1
+    public class SendMail
     {
+        public void SendTheMail(MailMessage email)
+        {
+
+        }
     }
 }
---
+++ PiOfTheTiger/PiOfTheTiger/PiOTTDAL/Entities/EmailSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiOTTDAL.Entities
+{
+    [Serializable]
+    public class EmailSettings
+    {
+        public int IdEmailSettings { get; set; }
+        public String EmailSettingsName { get; set; }
+        public String SMTPServer { get; set; }
+        public String SMTPUserName { get; set; }
+        public String SMTPPassword { get; set; }
+        public String SMTPSslRequired { get; set; }
+    }
+}
